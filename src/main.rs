901cfb256395d76ceea448b3fc886eeb83e876d1
Shamit187 <shamit187@gmail.com>
2024-02-21 16:40:27 +0600

    :) Table of Content Comes from Database. It's Cookin

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,13 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let address = format!("0.0.0.0:{}", port);
 
+    // Connect to the database
+    // let mongodb_uri = env::var("MONGOURI").expect("MONGODB_URI must be set");
+    // let client = mongodb::Client::with_uri_str(&mongodb_uri).await.unwrap();
+    // let collection = client.database("personal-bookmark").collection::<note::TableOfContent>("toc");
+    // let mut cursor = collection.find(doc! {"book_id" : 10}, None).await.unwrap();
+    // let table_of_content = cursor.try_next().await.unwrap().unwrap();
+    // println!("Title: {}", table_of_content.title);
 
     // Start the server
     println!("Starting server on: {}", &address);
--- src/note.rs
+++ src/note.rs
@@ -1,6 +1,5 @@
 use actix_web::{get, web, HttpResponse, Result};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::fs;
 use std::env;
 use futures::stream::TryStreamExt;
@@ -69,50 +68,42 @@ fn generate_note(contents: Vec<NoteContent>) -> Result<String, serde_json::Error
     Ok(text)
 }
 
-fn generate_table_of_content(json_str: &str) -> String {
-    let data: Value = serde_json::from_str(json_str).unwrap();
-    let title = data["title"].as_str().unwrap_or_default();
-    let book_id = data["book_id"].as_u64().unwrap_or_default();
+fn generate_table_of_content(toc: &TableOfContent) -> String {
+    let title = &toc.title;
+    let book_id = toc.book_id;
     let mut html = format!(
         "<h1 class=\"text-3xl p-2 dark:text-gray-200\">{}</h1>",
         title
     );
 
-    if let Some(chapters) = data["chapter"].as_array() {
-        for (chapter_idx, chapter) in chapters.iter().enumerate() {
-            let chapter_title = chapter["chapter_title"].as_str().unwrap_or_default();
-            let chapter_href = format!("/notes/{}/{}/0", book_id, chapter_idx);
-            html.push_str(&format!(
-                "<a href=\"{}\" class=\"block px-2 py-4 dark:text-gray-200 text-xl\">Chapter {}: {}</a>",
-                chapter_href,
-                chapter_idx + 1,
-                chapter_title
-            ));
+    for (chapter_idx, chapter) in toc.chapter.iter().enumerate() {
+        let chapter_title = &chapter.chapter_title;
+        let chapter_href = format!("/notes/{}/{}/0", book_id, chapter_idx);
+        html.push_str(&format!(
+            "<a href=\"{}\" class=\"block px-2 py-4 dark:text-gray-200 text-xl\">Chapter {}: {}</a>",
+            chapter_href,
+            chapter_idx + 1,
+            chapter_title
+        ));
 
-            if let Some(subchapters) = chapter["subchapter"].as_array() {
-                for (subchapter_idx, subchapter) in subchapters.iter().enumerate() {
-                    let subchapter_title =
-                        subchapter["subchapter_title"].as_str().unwrap_or_default();
-                    let subchapter_href =
-                        format!("/notes/{}/{}/{}", book_id, chapter_idx, subchapter_idx);
-                    html.push_str(&format!(
-                        "<a href=\"{}\" class=\"block p-2 dark:text-gray-200 text-base\">{}.{}: {}</a>",
-                        subchapter_href,
-                        chapter_idx + 1,
-                        subchapter_idx + 1,
-                        subchapter_title
-                    ));
-                }
-            }
+        for (subchapter_idx, subchapter) in chapter.subchapter.iter().enumerate() {
+            let subchapter_title = &subchapter.subchapter_title;
+            let subchapter_href = format!("/notes/{}/{}/{}", book_id, chapter_idx, subchapter_idx);
+            html.push_str(&format!(
+                "<a href=\"{}\" class=\"block p-2 dark:text-gray-200 text-base\">{}.{}: {}</a>",
+                subchapter_href,
+                chapter_idx + 1,
+                subchapter_idx + 1,
+                subchapter_title
+            ));
         }
     }
 
     html
 }
-
 /// extract path info using serde
 #[get("/notes/{book_id}/{chapter}/{subchapter}")] // <- define path parameters
-async fn index(_info: web::Path<Info>) -> Result<HttpResponse> {
+async fn index(info: web::Path<Info>) -> Result<HttpResponse> {
     // Read the contents of the files
     let note_top_html =
         fs::read_to_string("template/note_top.txt").expect("Unable to read note_top.txt");
@@ -124,7 +115,7 @@ async fn index(_info: web::Path<Info>) -> Result<HttpResponse> {
     // Query the database using book_id, chapter, and subchapter
     println!(
         "Querying the database for book_id: {} chapter: {} subchapter: {}",
-        _info.book_id, _info.chapter, _info.subchapter
+        info.book_id, info.chapter, info.subchapter
     );
 
     // Read from database
@@ -132,25 +123,40 @@ async fn index(_info: web::Path<Info>) -> Result<HttpResponse> {
     let client = mongodb::Client::with_uri_str(&mongodb_uri).await.unwrap();
     let db = client.database("personal-bookmark");
     let collection = db.collection::<TableOfContent>("toc");
-    let mut cursor = collection.find(doc! {}, None).await.unwrap();
-    while let Some(result) = cursor.try_next().await.unwrap(){
-        println!("{:?}", result);
-    }
+    let mut cursor = collection.find(doc! {"book_id" : info.book_id}, None).await.unwrap();
+    let table_of_content = match cursor.try_next().await {
+        Ok(Some(toc)) => toc,
+        Ok(None) => {
+            println!("No table of content found for book_id: {}", info.book_id);
+            TableOfContent {
+                book_id: info.book_id,
+                title: String::from("No title found"),
+                chapter: vec![],
+            }
+        }
+        Err(e) => {
+            println!("Error querying the database: {}", e);
+            TableOfContent {
+                book_id: info.book_id,
+                title: String::from("No title found"),
+                chapter: vec![],
+            }
+        }
+    };
+    let toc_html = generate_table_of_content(&table_of_content);
 
     // In future all will come from database
-
     let json_note = fs::read_to_string("template/note_content.json")?;
     let note: Note = serde_json::from_str(&json_note)?;
     let note_html = generate_note(note.content)?;
-
-    let json_toc = fs::read_to_string("template/note_toc.json").unwrap();
-    let toc_html = generate_table_of_content(&json_toc);
 
     // Generate HTML content dynamically
     let html_content = format!(
         "{} {} {} {} {}",
         note_top_html, toc_html, note_middle_html, note_html, note_bottom_content
     );
+
+    // Closing database connection
 
     Ok(HttpResponse::Ok()
         .content_type("text/html")
